a9070ad1e20376a83bb8a0679651b37e0c998b84
Victor kamau <Victor Wangari>
2024-05-30 19:18:25 +0300

    inital commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+     with open(f'{file_name}.txt', 'w') as f:
+         f.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
+    
 
 def read_file(file_name):
+    with open(f'{file_name}.txt') as f:
+        return f.read()
     pass
